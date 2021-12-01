440e3464a41e08aabf8ff68d71949af807261f27
Zak Thompson <zak@postscript.io>
2021-12-01 10:19:53 -0500

    Rust day 1 part 2

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -1,14 +1,14 @@
 use std::fs;
 
 fn main() {
-    println!("Hello, world!");
-
     let f = fs::read_to_string("1.input").expect("Unable to open file!");
-    let lines = f.split("\n");
+    let lines = f.split("\n").collect::<Vec<&str>>();
     let mut previous_num = 0;
     let mut increases = 0;
-    for line in lines {
-        let num: i32 = line.parse().unwrap();
+    for i in 0..lines.len() - 2 {
+        let num = lines[i].parse::<i32>().unwrap()
+            + lines[i + 1].parse::<i32>().unwrap()
+            + lines[i + 2].parse::<i32>().unwrap();
         if previous_num == 0 {
             previous_num = num;
             continue;
