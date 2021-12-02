83c9b785d86e93ee6c5703e6f356964d9e44fdd7
Zak Thompson <zak@postscript.io>
2021-12-02 10:46:32 -0500

    Day 2 rust

---
+++ rust/day2/src/main.rs
@@ -0,0 +1,46 @@
+use std::fs;
+
+
+fn main() {
+    let f = fs::read_to_string("2.input").expect("Unable to open file!");
+    let lines = f.split("\n").map(|s| s.split(" ").collect::<Vec<&str>>()).collect::<Vec<Vec<&str>>>();
+
+    println!("{}", part_one(&lines));
+    println!("{}", part_two(lines));
+}
+
+
+fn part_one(instructions: &Vec<Vec<&str>>) -> i32{
+    let mut forward = 0;
+    let mut depth = 0;
+    for line in instructions {
+        let num: i32 = line[1].parse().unwrap();
+        let direction = line[0];
+        match direction {
+            "forward" => forward += num,
+            "down" => depth += num,
+            "up" => depth -= num,
+            _ => println!("not a direction"),
+        }
+    }
+
+    forward * depth
+}
+
+fn part_two(instructions: Vec<Vec<&str>>) -> i32{
+    let mut forward = 0;
+    let mut aim = 0;
+    let mut depth = 0;
+    for line in instructions {
+        let num: i32 = line[1].parse().unwrap();
+        let direction = line[0];
+        match direction {
+            "forward" => {forward += num; depth += aim * num},
+            "down" => aim += num,
+            "up" => aim -= num,
+            _ => println!("not a direction"),
+        }
+    }
+
+    forward * depth
+}
