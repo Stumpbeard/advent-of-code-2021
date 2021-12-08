7057cbc5dc6a6778bc323cf56113198a8dde2e75
Zak Thompson <zak@postscript.io>
2021-12-08 09:19:54 -0500

    Day 4 solution in rust

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, ops::Index};
 
 fn main() {
     let f = fs::read_to_string("sample").expect("Unable to open file!");
@@ -11,7 +11,8 @@ fn main() {
             .collect::<Vec<u64>>();
         let mut boards = get_boards(&lines[1..]);
 
-        part_one(&drawings, &mut boards);
+        println!("part 1: {}", part_one(&drawings, &mut boards));
+        println!("part 2: {}", part_two(&drawings, &mut boards, vec![]));
     }
 }
 
@@ -36,7 +37,41 @@ fn get_boards(lines: &[&str]) -> Vec<Vec<Vec<u64>>> {
     boards
 }
 
-fn part_one(drawings: &Vec<u64>, boards: &mut Vec<Vec<Vec<u64>>>) {
+fn check_win(board: &Vec<Vec<u64>>) -> bool {
+    for row in board {
+        let row_check = row.iter().filter(|n| **n == 0).collect::<Vec<&u64>>();
+        if row_check.len() == 5 {
+            return true;
+        }
+    }
+
+    for i in 0..board[0].len() {
+        let mut column = vec![];
+        for x in 0..board.len() {
+            let row = &board[x];
+            column.push(row[i]);
+        }
+        let column_check = column.iter().filter(|n| **n == 0).collect::<Vec<&u64>>();
+        if column_check.len() == 5 {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn calculate_score(board: &Vec<Vec<u64>>, num: &u64) -> u64 {
+    let mut unmarked_sum = 0;
+    for row in board {
+        for column in row {
+            unmarked_sum += column;
+        }
+    }
+
+    unmarked_sum * num
+}
+
+fn part_one(drawings: &Vec<u64>, boards: &mut Vec<Vec<Vec<u64>>>) -> u64 {
     for num in drawings {
         for x in 0..boards.len() {
             let board = &mut boards[x];
@@ -49,7 +84,42 @@ fn part_one(drawings: &Vec<u64>, boards: &mut Vec<Vec<Vec<u64>>>) {
                     }
                 }
             }
+            if check_win(board) {
+                return calculate_score(board, num);
+            }
         }
     }
-    println!("{:?}", boards);
+
+    0
+}
+
+fn part_two(drawings: &Vec<u64>, boards: &mut Vec<Vec<Vec<u64>>>, mut skips: Vec<usize>) -> u64 {
+    let boards_size = boards.len();
+    for num in drawings {
+        for x in 0..boards.len() {
+            if skips.contains(&x) {
+                continue;
+            }
+            let board = &mut boards[x];
+            for y in 0..board.len() {
+                let row = &mut board[y];
+                for z in 0..row.len() {
+                    let place = row[z];
+                    if place == *num {
+                        row[z] = 0;
+                    }
+                }
+            }
+            if check_win(board) {
+                if boards_size - skips.len() == 1 {
+                    return calculate_score(board, num);
+                } else {
+                    skips.push(x);
+                    return part_two(drawings, boards, skips);
+                }
+            }
+        }
+    }
+
+    0
 }
